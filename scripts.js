e39ef283b6b8bad1009efa549c2239de895a8eca
Nalam Manasa Venkata Naga Sai <nalammanasa2002@gmail.com>
2023-01-17 11:45:33 +0530

    fixes #6

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 // User seat selection
 const userSelection = [];
+
 // Seats that are already booked
-const bookedSeat = [{ seatId: "1-0" }, { seatId: "1-1" }, { seatId: "1-6" }];
+let bookedSeat = [];
+
+let file = "BookedSeats.json";
+fetch(file)
+  .then((response) => response.json())
+  .then(function (data) {
+    bookedSeat = data;
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("here", error);
+  });
 
 /**
  * Disable seat by ID
@@ -20,7 +32,21 @@ const disableSeat = function (id) {
  */
 const onClickSeat = function (e) {
   let seatId = e.target.id || "";
-  console.log(`on click seat with ID: ${seatId || "none"}`);
+
+  if (document.getElementById(seatId).hasAttribute("selected") === false) {
+    document.getElementById(seatId).setAttribute("selected", true);
+    userSelection.push({ seatId: seatId });
+    console.log(userSelection);
+    //console.log(`on click seat with ID: ${seatId || "none"}`);
+  } else {
+    document.getElementById(seatId).removeAttribute("selected");
+    let elementToBeRemoved = userSelection.find(
+      (item) => item.seatId === seatId
+    );
+    console.log(elementToBeRemoved);
+    userSelection.splice(userSelection.indexOf(elementToBeRemoved), 1);
+    console.log(userSelection);
+  }
 };
 
 /**
@@ -31,11 +57,30 @@ const registerSeatClickEvent = function (ele) {
   ele.addEventListener("click", onClickSeat);
 };
 
+const loadUserSelectedSeats = function () {  
+  if ( localStorage.getItem("userSelection") ){    
+    let arr = localStorage.getItem("userSelection").split(",");    
+    arr.forEach ((element) => {
+      userSelection.push( { "seatId" : element } )
+      document.getElementById(element).setAttribute("selected", true);
+    });
+  };
+}
+
 const main = function () {
   // Disable all booked seats
   bookedSeat.forEach(({ seatId }) => {
     disableSeat(seatId);
   });
+
+  //verifying the booking
+  checkCollision();
+
+  //enabling already booked seats
+  loadUserSelectedSeats();
+
+  //enabling the book button
+  enableBookButton();
 
   // Get all seat elements
   const eleSeats = document.getElementsByClassName("seat");
@@ -50,11 +95,57 @@ const main = function () {
     }
 
     registerSeatClickEvent(ele);
+
   }
+
+
+
 };
 
 // Wait until page is ready
 window.addEventListener("load", () => {
   // Page is ready
-  main();
+  fetch(file)
+    .then((response) => response.json())
+    .then(function (data) {
+      bookedSeat = data;
+      main();
+      //console.log(bookedSeat);
+    })
+    .catch((error) => {
+      console.error("here", error);
+    });
+
 });
+
+let clearButton = document.getElementById("clear");
+clearButton.onclick = function(){
+  localStorage.clear();
+  location.reload();
+}
+
+const checkCollision = function () {
+  let BSeat = []
+  let USSeat = []
+  bookedSeat.forEach ( ( element ) => {
+    BSeat.push( Object.values( element ) );
+  });
+  userSelection.forEach ( ( element ) => {
+    USSeat.push( Object.values( element ) );
+  });
+  for ( let i = 0 ; i < BSeat.length ; i++ ){
+
+    for ( let j = 0 ; j < USSeat.length ; j++ ){
+
+      if( BSeat[i] === USSeat[j] ){
+
+        let elementToBeRemoved = userSelection.find(
+          (item) => item.seatId === USSeat(j)
+        );
+        userSelection.splice( userSelection.indexOf(elementToBeRemoved), 1 );
+
+      }
+
+    }
+  }
+}
